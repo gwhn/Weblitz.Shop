06a09c32c10da0e14bf8b82b6cacdc72c8efe0df
Guy Newman <gwhn@weblitz.co.uk>
2013-06-19 14:54:51 +0100

    Improved product list layout by removing media

--- Weblitz.Shop.Web/Filters/CategoryFilterAttribute.cs
+++ Weblitz.Shop.Web/Filters/CategoryFilterAttribute.cs
@@ -73,6 +73,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 1,
                                             Title = "Featured Product 1",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             Price = 1.23M,
                                             DicountedPrice = 2.34M,
                                             IsNew = true,
@@ -91,6 +92,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 2,
                                             Title = "Featured Product 2",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             Price = 2.34M,
                                             IsNew = true,
                                             Image = new Image
@@ -108,6 +110,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 3,
                                             Title = "Featured Product 3",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             Price = 3.45M,
                                             DicountedPrice = 4.56M,
                                             Image = new Image
@@ -125,6 +128,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 4,
                                             Title = "Featured Product 4",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             Price = 4.56M,
                                             Image = new Image
                                                 {
@@ -141,6 +145,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 5,
                                             Title = "Featured Product 5",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             Price = 5.67M,
                                             Image = new Image
                                                 {
@@ -157,6 +162,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 6,
                                             Title = "Featured Product 6",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             Price = 6.78M,
                                             Image = new Image
                                                 {
@@ -173,6 +179,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 7,
                                             Title = "Latest Product 7",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             IsNew = true,
                                             Price = 1.23M,
                                             Image = new Image
@@ -190,6 +197,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 8,
                                             Title = "Latest Product 8",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             IsNew = true,
                                             Price = 2.34M,
                                             Image = new Image
@@ -207,6 +215,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 9,
                                             Title = "Latest Product 9",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             IsNew = true,
                                             Price = 3.45M,
                                             Image = new Image
@@ -224,6 +233,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 10,
                                             Title = "Latest Product 10",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             IsNew = true,
                                             Price = 4.56M,
                                             Image = new Image
@@ -241,6 +251,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 11,
                                             Title = "Latest Product 11",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             IsNew = true,
                                             Price = 5.67M,
                                             Image = new Image
@@ -258,6 +269,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 12,
                                             Title = "Latest Product 12",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             IsNew = true,
                                             Price = 6.78M,
                                             Image = new Image
@@ -275,6 +287,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 13,
                                             Title = "Sale Product 13",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             Price = 2.34M,
                                             DicountedPrice = 1.23M,
                                             Image = new Image
@@ -292,6 +305,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 14,
                                             Title = "Sale Product 14",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             Price = 3.45M,
                                             DicountedPrice = 2.34M,
                                             Image = new Image
@@ -309,6 +323,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 15,
                                             Title = "Sale Product 15",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             Price = 4.56M,
                                             DicountedPrice = 3.45M,
                                             Image = new Image
@@ -326,6 +341,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 16,
                                             Title = "Sale Product 16",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             Price = 5.67M,
                                             DicountedPrice = 4.56M,
                                             Image = new Image
@@ -343,6 +359,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 17,
                                             Title = "Sale Product 17",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             Price = 6.78M,
                                             DicountedPrice = 5.67M,
                                             Image = new Image
@@ -360,6 +377,7 @@ namespace Weblitz.Shop.Web.Filters
                                         {
                                             Id = 18,
                                             Title = "Sale Product 18",
+                                            Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit fusce vel sapien elit in malesuada semper mi, id sollicitudin urna fermentum ut fusce varius nisl ac ipsum gravida vel pretium tellus.",
                                             Price = 7.89M,
                                             DicountedPrice = 6.78M,
                                             Image = new Image
